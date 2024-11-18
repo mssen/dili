9259be9ab73ae80695fa8799ca4e17c0d9dd27e2
Missy Danielsen <missy.danielsen@gmail.com>
2024-11-17 19:56:01 -0600

    feat(rating): create custom rating component

--- src/components/Rating/Rating.vue
+++ src/components/Rating/Rating.vue
@@ -1,18 +1,41 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, useTemplateRef, watchEffect } from 'vue';
 import Star, { type RatingValue } from './Star.vue';
 
 const rating = ref<RatingValue | undefined>(undefined);
+
+const hoveredValue = ref<RatingValue | undefined>(undefined);
+const oneRef = useTemplateRef('one');
+const twoRef = useTemplateRef('two');
+const threeRef = useTemplateRef('three');
+const fourRef = useTemplateRef('four');
+const fiveRef = useTemplateRef('five');
+
+watchEffect(() => {
+  if (oneRef.value?.isHovered) {
+    hoveredValue.value = 1;
+  } else if (twoRef.value?.isHovered) {
+    hoveredValue.value = 2;
+  } else if (threeRef.value?.isHovered) {
+    hoveredValue.value = 3;
+  } else if (fourRef.value?.isHovered) {
+    hoveredValue.value = 4;
+  } else if (fiveRef.value?.isHovered) {
+    hoveredValue.value = 5;
+  } else {
+    hoveredValue.value = undefined;
+  }
+});
 </script>
 
 <template>
   <p>Rating</p>
   <div :class="$style.container">
-    <Star v-model="rating" :value="1" />
-    <Star v-model="rating" :value="2" />
-    <Star v-model="rating" :value="3" />
-    <Star v-model="rating" :value="4" />
-    <Star v-model="rating" :value="5" />
+    <Star v-model="rating" ref="one" :value="1" :hovered-value="hoveredValue" />
+    <Star v-model="rating" ref="two" :value="2" :hovered-value="hoveredValue" />
+    <Star v-model="rating" ref="three" :value="3" :hovered-value="hoveredValue" />
+    <Star v-model="rating" ref="four" :value="4" :hovered-value="hoveredValue" />
+    <Star v-model="rating" ref="five" :value="5" :hovered-value="hoveredValue" />
   </div>
 
   <p>Value: {{ rating }}</p>
--- src/components/Rating/Star.vue
+++ src/components/Rating/Star.vue
@@ -1,29 +1,50 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps, ref } from 'vue';
 import { Icon } from '@iconify/vue';
+import { useElementHover } from '@vueuse/core';
 
 export type RatingValue = 1 | 2 | 3 | 4 | 5;
 
-const props = defineProps<{ value: RatingValue }>();
+const props = defineProps<{ value: RatingValue; hoveredValue: RatingValue | undefined }>();
 const currentValue = defineModel<RatingValue | undefined>();
 const id = `star${props.value}`;
+
+const starElement = ref();
+const isHovered = useElementHover(starElement);
+defineExpose({
+  isHovered,
+});
 </script>
 
 <template>
   <input v-model="currentValue" :value="value" :id="id" type="radio" name="rating" class="visually-hidden" />
   <label :for="id">
     <span class="visually-hidden">{{ value }} star{{ value > 1 ? 's' : '' }}</span>
-    <div :class="$style.icon">
+    <div :class="{ icon: true, hover: hoveredValue && value <= hoveredValue }" ref="starElement">
       <Icon v-if="currentValue === undefined" icon="iconoir:star-dashed" />
-      <Icon v-else-if="currentValue >= value" icon="iconoir:star-solid" />
+      <Icon
+        :class="{ selected: true, hover: hoveredValue && value > hoveredValue }"
+        v-else-if="currentValue >= value"
+        icon="iconoir:star-solid"
+      />
       <Icon v-else icon="iconoir:star" />
     </div>
   </label>
 </template>
 
-<style lang="css" module>
+<style lang="css" scoped>
 .icon {
   padding: var(--size-1);
   font-size: 1.2em;
+}
+
+.icon:hover,
+.hover,
+.selected {
+  color: var(--color-primary);
+}
+
+.selected.hover {
+  color: inherit;
 }
 </style>
