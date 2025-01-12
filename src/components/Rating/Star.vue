9a5cbb3aaa60f9eb14f81541d4191a3b9eca613b
Missy Danielsen <missy.danielsen@gmail.com>
2025-01-11 20:45:44 -0600

    chore: remove unneeded import

--- src/components/Rating/Star.vue
+++ src/components/Rating/Star.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, ref } from 'vue';
+import { ref } from 'vue';
 import { Icon } from '@iconify/vue';
 import { useElementHover } from '@vueuse/core';
 
